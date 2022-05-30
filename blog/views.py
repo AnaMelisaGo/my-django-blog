e6b0e7f5bcf440f5949919c51ca9d752a56b91b4
AnaMelisaGo <melisago.rockn@gmail.com>
2022-05-30 16:39:15 +0000

    Add Post view in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class Postlist(generic.ListView):
+    """
+    Generic list view of Post model
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    paginate_by = 6 # separate objects into 6 page
